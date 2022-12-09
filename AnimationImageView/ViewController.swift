7f8893e1d53b1dc2aa3df9d483f6fd0ff7ea9435
BorisSonin <bsonin85@gmail.com>
2022-12-09 20:15:44 +0300

    Initial Commit

---
+++ AnimationImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationImageView
+//
+//  Created by Борис Сонин on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
